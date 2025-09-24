5d4eaf56bb51f87a593c17f3c8e0d2e86570cdb7
pradeep-iitb <pradeepkumawat132005@gmail.com>
2025-09-24 16:29:39 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar';
-
+import Hero from './components/Hero'
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
@@ -9,6 +9,8 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
     </main>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { navLinks } from '../../constants/index.js'
 const Navbar = () => {
+useGSAP(()=> {
+    const navTween = gsap.timeline({
+        scrollTrigger: {
+            trigger : 'nav' ,
+            start : 'bottom top' ,
+        }
+    }) ;
+    
+    navTween.fromTo('nav', { backgroundColor: 'transparent' } , {
+        backgroundColor: '#00000050' ,
+        backgroundFilter: 'blur(10px)' ,
+        duration:1 ,
+        ease: 'power1.inOut'
+    });
+})
+
   return (
     <nav>
         <div>
             <a href='#home' className='flex items-center gap-2'>
+                <img src="/images/logo.png" alt="Velvet Pour" />
                 <p>Velvet Pour</p>
             </a>
             <ul>
