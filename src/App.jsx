c326842f1d57c88c9d8171ac7e79681b486fd392
pradeep-iitb <pradeepkumawat132005@gmail.com>
2025-09-26 02:36:25 +0530

    added a final contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
+import { useGSAP } from '@gsap/react'
 
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
