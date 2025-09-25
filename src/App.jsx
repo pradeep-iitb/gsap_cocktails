6f9af7f7a0bbd30c436a44a9cb4ef5729e1078b9
pradeep-iitb <pradeepkumawat132005@gmail.com>
2025-09-26 02:39:35 +0530

    Final app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
-import { useGSAP } from '@gsap/react'
 
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
